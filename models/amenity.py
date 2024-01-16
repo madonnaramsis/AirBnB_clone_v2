07c7612d3704efd95b933546be996eb7edee095d
Mohamed Kamal <ku18mz@gmail.com>
2024-01-16 18:37:31 +0200

    pycs standards

--- models/amenity.py
+++ models/amenity.py
@@ -4,5 +4,5 @@ from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """ Amenity class """
+    """Amenity class representing a feature or service in a facility."""
     name = ""
