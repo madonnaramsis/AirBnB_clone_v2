46dd7774ef533923bdde9df32c85b84c8461a3ad
Mohamed Kamal <ku18mz@gmail.com>
2024-01-18 01:11:14 +0200

    Refactor tests and add method documentation
    The code refactor improves test clarity and adds method documentation to
    enhance code maintainability and readability.
    
    #refactor #documentation

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -9,7 +9,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 from models import DBStorage
-import os
+
 
 
 
@@ -17,15 +17,18 @@ class test_DBStorage(unittest.TestCase):
     """ Class to test the file storage method """
 
     def test_instantiation_without_errors(self):
+        """ test instantiation of the DBStorage class """
         db_storage = DBStorage()
         self.assertEqual(str(type(db_storage)), "<class 'models.db_storage.DBStorage'>")
 
     def test_query_all_objects(self):
+        """ test the query all method """
         db_storage = DBStorage()
         results = db_storage.all()
         self.assertEqual(len(results), 0)
 
     def test_add_object_to_session(self):
+        """ test the add method """
         db_storage = DBStorage()
         state = State(name="California")
         db_storage.new(state)
@@ -33,25 +36,8 @@ class test_DBStorage(unittest.TestCase):
         results = db_storage.all(State)
         self.assertEqual(len(results), 1)
 
-#!/usr/bin/python3
-""" Module for testing file storage"""
-import unittest
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-from models import DBStorage
-import os
-
-
-
-class test_DBStorage(unittest.TestCase):
-    """ Class to test the file storage method """
-
     def test_query_all_objects_with_class_specified(self):
+        """ test the query all method """
         db_storage = DBStorage()
         state = State(name="California")
         db_storage.new(state)
@@ -60,6 +46,7 @@ class test_DBStorage(unittest.TestCase):
         self.assertEqual(len(results), 1)
 
     def test_delete_object_from_session(self):
+        """ test the delete method """
         db_storage = DBStorage()
         state = State(name="California")
         db_storage.new(state)
@@ -69,6 +56,7 @@ class test_DBStorage(unittest.TestCase):
         self.assertEqual(len(results), 0)
 
     def test_add_city_to_session(self):
+        """ test the add method """
         db_storage = DBStorage()
         city = City(name="San Francisco", state_id=1)
         db_storage.new(city)
@@ -77,6 +65,7 @@ class test_DBStorage(unittest.TestCase):
         self.assertEqual(len(results), 1)
 
     def test_query_objects_with_class_specified(self):
+        """ test the query all method """
         db_storage = DBStorage()
         state = State(name="California")
         db_storage.new(state)
@@ -85,6 +74,7 @@ class test_DBStorage(unittest.TestCase):
         self.assertEqual(len(results), 1)
 
     def test_delete_nonexistent_object(self):
+        """test deleting nonexistent object"""
         db_storage = DBStorage()
         state = State(name="California")
         db_storage.delete(state)
@@ -93,34 +83,40 @@ class test_DBStorage(unittest.TestCase):
         self.assertEqual(len(results), 0)
 
     def test_reload_without_creating_tables(self):
+        """test reloading without creating tables"""
         db_storage = DBStorage()
         db_storage.reload()
         results = db_storage.all(State)
         self.assertEqual(len(results), 0)
 
     def test_reload_with_creating_tables(self):
+        """test reloading with creating tables"""
         db_storage = DBStorage()
         db_storage.reload()
         results = db_storage.all(State)
         self.assertEqual(len(results), 0)
+        db_storage.new(State(name="California"))
+        db_storage.save()
+        db_storage.reload()
+        results = db_storage.all(State)
+        self.assertEqual(len(results), 1)
 
-    def test_add_object_to_session(self):
+    def test_add_amenity_to_session(self):
+        """ test the add method """
+        db_storage = DBStorage()
+        amenity = Amenity(name="Wifi")
+        db_storage.new(amenity)
+        db_storage.save()
+        results = db_storage.all(Amenity)
+        self.assertEqual(len(results), 1)
+
+    def test_delete_already_deleted_object(self):
+        """test deleting already deleted object"""
         db_storage = DBStorage()
         state = State(name="California")
         db_storage.new(state)
         db_storage.save()
+        db_storage.delete(state)
+        db_storage.delete(state)
         results = db_storage.all(State)
-        self.assertEqual(len(results), 1)
-
-    def test_query_all_objects(self):
-        db_storage = DBStorage()
-        results = db_storage.all()
         self.assertEqual(len(results), 0)
-
-    def test_add_object_to_session(self):
-        db_storage = DBStorage()
-        state = State(name="California")
-        db_storage.new(state)
-        db_storage.save()
-        results = db_storage.all(State)
-        self.assertEqual(len(results), 1)
