3f41cab8d6b1ea40b7227ef95d17a2de59978d9e
Mohamed Kamal <ku18mz@gmail.com>
2024-01-17 22:50:28 +0200

    Refactor relationships in models for cascading delete
    Update relationships in user, place, and review models to support
    cascading delete and delete-orphan, ensuring proper handling of related
    data when parent entities are removed. Improves data consistency and
    maintenance. #123

--- models/place.py
+++ models/place.py
@@ -18,4 +18,14 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float)
     longitude = Column(Float)
+    reviews = relationship("Review", backref="place", cascade="delete")
     amenity_ids = []
+
+    @property
+    def reviews(self):
+        """Returns the list of Review instances with place_id
+        equals to the current Place.id
+        """
+        from models import storage
+        from models.review import Review
+        return storage.all(Review).values()
