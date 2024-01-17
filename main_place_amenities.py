09dda946da5183121c184d12259919df4fe2a73b
Madona Adeeb <madona.adeeb@hotmail.com>
2024-01-18 01:48:24 +0200

    Refactor user and place creation logic
    Consolidated user and place creation into a single process for improved
    code organization and readability. This change reduces redundancy and
    simplifies maintenance.
    
    #123

--- main_place_amenities.py
+++ main_place_amenities.py
@@ -15,13 +15,3 @@ state.save()
 # creation of a City
 city = City(state_id=state.id, name="San Francisco")
 city.save()
-
-# creation of a User
-user = User(email="john@snow.com", password="johnpwd")
-user.save()
-
-# creation of 2 Places
-place_1 = Place(user_id=user.id, city_id=city.id, name="House 1")
-place_1.save()
-place_2 = Place(user_id=user.id, city_id=city.id, name="House 2")
-place_2.save()
