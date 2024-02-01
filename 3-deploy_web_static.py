1702ce89b1736fe4022116a6d12b33be7b6d35bc
Mohamed Kamal <ku18mz@gmail.com>
2024-02-02 01:48:18 +0200

    reverse the version

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,51 +2,52 @@
 """Fabric script that generates a .tgz archive
 from the contents of the web_static folder
 using the function do_pack."""
-from fabric.api import local
-from fabric.operations import env, put, run
-import time
-from os.path import isfile
+from fabric.api import env, put, run, local
+from datetime import datetime
+from os.path import exists
 env.hosts = ['54.210.173.51', '52.73.37.123']
+env.user = 'ubuntu'
 
 
 def do_pack():
     """Function to generate a .tgz archive."""
+    local("mkdir -p versions")
+    date = datetime.now()
+    date = date.strftime("%Y%m%d%H%M%S")
     try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".
-              format(time.strftime("%Y%m%d%H%M%S")))
-        return ("versions/web_static_{}.tgz".format(time.
-                                                    strftime("%Y%m%d%H%M%S")))
-    except:
+        local("tar -cvzf versions/web_static_{}.tgz web_static".format(date))
+        return "versions/web_static_{}.tgz".format(date)
+    except Exception:
         return None
 
 
 def do_deploy(archive_path):
     """Function to deploy."""
-    if (isfile(archive_path) is False):
+    if not archive_path and not exists(archive_path):
         return False
-
     try:
-        file = archive_path.split("/")[-1]
-        folder = ("/data/web_static/releases/" + file.split(".")[0])
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(folder))
-        run("tar -xzf /tmp/{} -C {}".format(file, folder))
+        file = archive_path.split("/")[-1]
+        name = file.split(".")[0]
+        run("mkdir -p /data/web_static/releases/{}/".format(name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(file, name))
         run("rm /tmp/{}".format(file))
-        run("mv {}/web_static/* {}/".format(folder, folder))
-        run("rm -rf {}/web_static".format(folder))
-        run('rm -rf /data/web_static/current')
-        run("ln -s {} /data/web_static/current".format(folder))
+        run("mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(name))
         print("New version deployed!")
         return True
-    except:
+    except Exception:
         return False
 
 
 def deploy():
     """Function to full deploy process."""
-    try:
-        path = do_pack()
-        return do_deploy(path)
-    except:
+    path = do_pack()
+    if not path:
         return False
+    return do_deploy(path)
